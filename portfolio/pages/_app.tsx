c7fa6e47652814192d51826647c0459ed23ad1ba
lechiffre4 <toxcoa@gmail.com>
2024-04-02 18:38:37 -0400

    New sidebar

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,12 +1,15 @@
 
 import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react';
-import Nav from '../components/navbar';
+import { useState } from 'react'
+import { Box, useBreakpointValue } from '@chakra-ui/react'
+import Sidebar from "../components/sidebar"
 
 export default function App({ Component, pageProps }: AppProps) {
+  
   return (
     <ChakraProvider>
-      <Nav/>
+      <Sidebar/>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,7 +3,7 @@ import { Text } from '@chakra-ui/react'
 export default function Home() {
   return (
     <>
-      <Text>Test</Text>
+    <Text>test</Text>
     </>
   );
 }
